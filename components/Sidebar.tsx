29154684ccb20e63429747b54a152b826ff8507f
zicoz18 <zicoz18@ku.edu.tr>
2022-01-12 15:20:09 +0300

    Add initial structure

---
+++ components/Sidebar.tsx
@@ -0,0 +1,17 @@
+/* _rfce command to create the template */
+import { HomeIcon, SearchIcon, LibraryIcon, PlusCircleIcon } from "@heroicons/react/outline";
+
+function Sidebar() {
+    return (
+        <div>
+
+            <div>
+                <button>
+                    <HomeIcon className="h-5 w-5" />
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
